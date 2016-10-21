474117d20aae6228b8fb5db7f64ba61b900e5515
dawn-knight <dawn_knight_xp@hotmail.com>
2016-10-21 15:12:39 +0800

    Tokenizer prototype completed 2016/10/21 afternoon

--- java/src/com/dk/cp/common/Tokenizer.java
+++ java/src/com/dk/cp/common/Tokenizer.java
@@ -6,37 +6,60 @@ import java.util.List;
 import com.dk.cp.common.MarkedVocabulary.TokenNode;
 
 public class Tokenizer {
-	private Vocabulary v;
+	private MarkedVocabulary mv;
 
-	public Vocabulary getV() {
-		return v;
+	public MarkedVocabulary getMv() {
+		return mv;
 	}
 
-	public void setV(Vocabulary v) {
-		this.v = v;
+	public void setMv(MarkedVocabulary mv) {
+		this.mv = mv;
 	}
 
 	public List<String> process(String raw) {
 		List<String> processed = new ArrayList<String>();
 		if (raw != null && raw.length() > 0) {
-
+			for (int i = 0; i < raw.length();) {
+				List<Integer> endPosL = getEndPosL(raw, i, mv);
+				if (endPosL != null) {
+					int nextStartPos = i + 1;
+					for (Integer endPos : endPosL) {
+						processed.add(raw.substring(i, endPos));
+						nextStartPos = endPos;
+					}
+					i = nextStartPos;
+				} else {
+					processed.add(raw.substring(i, i + 1));
+					i++;
+				}
+			}
 		}
 		return processed;
 	}
 
-	private int getEndPos(String raw, int pos, Vocabulary v) {
+	private List<Integer> getEndPosL(String raw, int pos, MarkedVocabulary mv) {
 		if (raw != null && raw.length() > 0 && pos < raw.length()) {
+			List<Integer> endPosL = null;
 			String s = raw.substring(pos, pos + 1);
 			TokenNode stopNode = new TokenNode(s, true);
 			TokenNode nonStopNode = new TokenNode(s, false);
-			if (v.containsKey(stopNode)) {
-				getEndPos(raw, pos + 1, v.get(stopNode));
-			} else if (v.containsKey(nonStopNode)) {
-				getEndPos(raw, pos + 1, v.get(nonStopNode));
-			} else {
-				return pos;
+			List<Integer> latterEndPosL = null;
+			if (mv.containsKey(stopNode)) {
+				if (endPosL == null)
+					endPosL = new ArrayList<Integer>();
+				endPosL.add(pos + 1);
+				latterEndPosL = getEndPosL(raw, pos + 1, mv.get(stopNode));
+			} else if (mv.containsKey(nonStopNode)) {
+				latterEndPosL = getEndPosL(raw, pos + 1, mv.get(nonStopNode));
 			}
+			if (latterEndPosL != null) {
+				if (endPosL == null)
+					endPosL = new ArrayList<Integer>();
+				endPosL.addAll(latterEndPosL);
+			}
+			return endPosL;
 		}
-		return 0;
+		return null;
 	}
+
 }
---
+++ java/test/com/dk/cp/common/TokenizerTest.java
@@ -0,0 +1,67 @@
+package com.dk.cp.common;
+
+import static org.junit.Assert.*;
+
+import java.util.List;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class TokenizerTest {
+	
+	private static MarkedVocabulary mv;
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		mv = new MarkedVocabulary();
+		mv.expand("中国");
+		mv.expand("中国人");
+		mv.expand("火星");
+		mv.expand("火星人");
+		mv.expand("殖民");
+		mv.expand("殖民地");
+		mv.expand("一定");
+		mv.expand("作为");
+		mv.expand("代表");
+		mv.expand("费晔晖");
+		mv.expand("潜力");
+		mv.expand("科技");
+		mv.expand("创新");
+		mv.expand("创新力");
+		mv.expand("企业");
+		mv.expand("领导者");
+		mv.expand("领导");
+		mv.expand("美利坚合众国");
+		mv.expand("美国");
+		mv.expand("美国人");
+		mv.expand("全称");
+	}
+
+	@Test
+	public void testOnylHan1() {
+		Tokenizer t = new Tokenizer();
+		t.setMv(mv);
+		List<String> processed = t.process("我作为一个中国人一定要代表中国殖民火星");
+		System.out.println(processed);
+		assertEquals(12, processed.size());
+	}
+
+	@Test
+	public void testOnlyHan2() {
+		Tokenizer t = new Tokenizer();
+		t.setMv(mv);
+		List<String> processed = t.process("费晔晖是中国最有潜力的科技创新企业的领导者");
+		System.out.println(processed);
+		assertEquals(13, processed.size());
+	}
+
+	@Test
+	public void testOnlyHan3() {
+		Tokenizer t = new Tokenizer();
+		t.setMv(mv);
+		List<String> processed = t.process("美国的全称是美利坚合众国");
+		System.out.println(processed);
+		assertEquals(5, processed.size());
+	}
+
+}
